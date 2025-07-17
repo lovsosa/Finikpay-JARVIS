463f2109067a8a2cf2846f2cde4bc09c467f246d
Melis <tyn.melis.ru@gmail.com>
2025-07-17 10:35:39 +0600

    update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,6 @@
 import 'dotenv/config';
 import express from 'express';
 import finikRouter from './src/routes/finikRoutes.js';
-import ngrok from 'ngrok';
 
 const app = express();
 app.use(express.json());
@@ -9,18 +8,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(finikRouter);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, async () => {
-  console.log(`Finik handler на порту ${PORT}`);
-
-  try {
-    const url = await ngrok.connect({
-      proto: 'http',
-      addr: PORT,
-      authtoken: process.env.NGROK_AUTHTOKEN,
-      hostname: process.env.NGROK_HOST,
-    });
-    console.log(`ngrok tunnel доступен по адресу: ${url}`);
-  } catch (err) {
-    console.error('Не удалось запустить ngrok:', err);
-  }
+app.listen(PORT, () => {
+  console.log(`Finik handler запущен на порту ${PORT}`);
 });
